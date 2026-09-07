f3782ba470934faa6db2b67d1ec9c8095565b351
Igor Kruzhilin <igorkruzhilin@gmail.com>
2026-09-07 11:17:47 +0000

    Add staging model for MOEX history with grain tests

---
+++ dbt/models/staging/stg_moex_history.sql
@@ -0,0 +1,31 @@
+select
+	concat(cast(dt as string), '|', board, '|', secid) history_key
+	, market market
+	, board board
+	, dt dt
+	, secid sec_id
+	, boardid board_id
+	, tradedate trade_date
+	, trade_session_date trade_session_date
+	, tradingsession trading_session
+	, shortname short_name
+	, currencyid currency_id
+	, open open_price
+	, high high_price
+	, low low_price
+	, close close_price
+	, legalcloseprice legal_close_price
+	, waprice wa_price
+	, marketprice2 market_price_2
+	, marketprice3 market_price_3
+	, admittedquote admitted_quote
+	, trendclspr trend_close_price
+	, cast(numtrades as int64) num_trades
+	, cast(volume as int64) volume
+	, value trade_value
+	, mp2valtrd mp2_value_traded
+	, marketprice3tradesvalue market_price_3_value
+	, admittedvalue admitted_value
+	, waval wa_value
+	, _extracted_at extracted_at
+from {{ source('raw', 'moex_history') }}
